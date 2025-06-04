17bbeb7b0e93619fe9fcea0475261fd493660afd
Lucien Bangelesa <ir.bangelesa@gmail.com>
2025-06-04 02:29:12 +0100

    exposing dist

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -30,7 +30,7 @@ const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
                     </g>
                 </svg>
 
-                <span>SAKAI</span>
+                <span>EKIN</span>
             </router-link>
         </div>
 
